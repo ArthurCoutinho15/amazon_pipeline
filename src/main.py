361e59b2701bbdf85cd4ea65caf90a4baed573cc
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-04-21 00:38:12 -0300

    feat: termina tratametos spark e iniciar carga para mysql

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from extract import Extract
 from mongo import Mongo
+from spark import Spark
 import json
 import os
 from dotenv import load_dotenv
@@ -30,7 +31,21 @@ def load_data_into_mongo(uri, db_name, coll_name,path):
     
     result = mongo.load(data)
     print(result)
+
+def transform_data(path, country, currency_symbol):
+    spark = Spark()
+    spark_session = spark.sparkSession()
+    data = spark.transform_data_into_json_lines(path)
+    new_path = spark.save_json_lines(data, path)
     
+    df = spark.transform_data(spark_session, country=country, currency_symbol=currency_symbol, path=new_path)
+    print("Dados transformados com sucesso!")
+    print(df.show(5))
+    return df
+
+
+
+
 if __name__ == "__main__":
     url_products_by_category = "https://real-time-amazon-data.p.rapidapi.com/products-by-category"
     url_products = "https://real-time-amazon-data.p.rapidapi.com/search"
@@ -42,3 +57,6 @@ if __name__ == "__main__":
     for country in ["US", "BR"]:
         path = extract_data_for_country(endpoint="products", query="Iphone", country=country, url=url_products, pages=1, file_name="produtos")
         load_data_into_mongo(uri=str(os.getenv('uri')), db_name="amazon_pipeline", coll_name="amazon_products", path=path)
+    
+    path = "/home/arthur/Projetos/amazon_pipeline/data/produtos_1_BR_copy.json"
+    df = transform_data(path, "BR", "R$")
--- src/spark.py
+++ src/spark.py
@@ -2,9 +2,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lit, regexp_replace, when
 import json
 
+
 class Spark():
-    
-    
+
     @staticmethod
     def sparkSession():
         spark = SparkSession\
@@ -13,20 +13,61 @@ class Spark():
             .config("spark.jars", "mysql-connector-java-8.0.28.jar")\
             .getOrCreate()
         return spark
-    
+
     def transform_data_into_json_lines(self, path):
         with open(path, "r") as f:
             raw_data = json.load(f)
-        
+
         data = raw_data["data"]["products"]
+
+        return data
 
     def save_json_lines(self, data, path):
         with open(path, "w") as f:
             for d in data:
-                f.write(json.dumsps(d) + "\n")
-
-    def transform_data(self,spark, country, currency_symbol, path):
-        df = spark.read_json(path)
-        df = df.withColumn("product_price", regexp_replace(col("product_price"), f"[{currency_symbol} ,]", "").cast("float"))\
-        .drop("is_best_seller", "is_amazon_choice", "is_prime", "product_badge", "climate_pledge_friendly")
+                f.write(json.dumps(d) + "\n")
         
+        return path
+
+    def transform_data(self, spark, country, currency_symbol, path):
+        """Realiza transformações nos dados usando PySpark."""
+        df = spark.read.json(path)
+
+        drop_cols = ["is_best_seller", "is_amazon_choice", "is_prime",
+                     "product_badge", "climate_pledge_friendly", "product_availability", "delivery", "has_variations",
+                     "coupon_text", "book_format"]
+
+        df = df.withColumn("product_price", when(col("product_price").isNull(), 0)
+                           .otherwise(regexp_replace(col("product_price"), f"[{currency_symbol} ,]", "").cast("float"))) \
+               .withColumn("product_original_price", when(col("product_original_price").isNull(), 0)
+                           .otherwise(regexp_replace(col("product_original_price"), f"[{currency_symbol} ,]", "").cast("float"))) \
+               .withColumn("product_minimum_offer_price", when(col("product_minimum_offer_price").isNull(), 0)
+                           .otherwise(regexp_replace(col("product_minimum_offer_price"), f"[{currency_symbol} ,]", "").cast("float"))) \
+               .withColumn("product_star_rating", col("product_star_rating").cast("float")) \
+               .withColumn("product_num_offers", col("product_num_offers").cast("float")) \
+               .withColumn("product_num_ratings", col("product_num_ratings").cast("float")) \
+               .withColumn("country", lit(country))
+
+        if country == "BR":
+            df = df.filter(col("product_title").startswith("Apple iPhone"))
+
+        df = df.drop(*[c for c in drop_cols if c in df.columns])
+        return df
+        
+    def join_df(self, df1, df2):
+        """Une dois DataFrames com as mesmas colunas."""
+        return df1.unionByName(df2)
+
+    def write_to_mysql(self, df, table_name, user, password, host="localhost", port=3306, database="meu_banco"):
+        """Salva o DataFrame no banco de dados MySQL."""
+        url = f"jdbc:mysql://{host}:{port}/{database}?useSSL=false&serverTimezone=UTC"
+
+        df.write \
+            .format("jdbc") \
+            .option("url", url) \
+            .option("dbtable", table_name) \
+            .option("user", user) \
+            .option("password", password) \
+            .option("driver", "com.mysql.cj.jdbc.Driver") \
+            .mode("append")\
+            .save()
